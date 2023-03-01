9f147ef6df2c7a89cf5b4ee4b91171f798c7a1ac
Alkida Vaci <alkida.vaci@gmail.com>
2023-03-01 18:24:38 -0500

    Add logic to a method that accepts some content and adds it to the database

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,7 +13,21 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  console.error('putDb not implemented');
+
+  // Create connection to database
+  const jateDb = await openDB('jate', 1);
+  // Create a transaction by transaction method from database that takes in name of db and the action we want to perform 
+  const tx = jateDb.transaction('jate', 'readwrite');
+  // Open up the desired object store
+  const store = tx.objectStore('jate');
+  // Update the content in the object store using the put method
+  const request = store.put({ id: id, jate: content });
+  // Get confirmation of the request
+  const result = await request;
+  console.log('🚀 - data saved to the database', result);
+} 
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => console.error('getDb not implemented');
