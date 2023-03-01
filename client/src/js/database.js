fa1cdf55c3f62b3acfd225cbef19cec0ea618618
Alkida Vaci <alkida.vaci@gmail.com>
2023-03-01 18:28:54 -0500

    Add logic for a method that gets all the content from the database

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,7 +12,7 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
+// Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
   console.error('putDb not implemented');
 
@@ -27,9 +27,24 @@ export const putDb = async (content) => {
   // Get confirmation of the request
   const result = await request;
   console.log('🚀 - data saved to the database', result);
-} 
+}
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+// Add logic for a method that gets all the content from the database
+export const getDb = async () => {
+  console.error('getDb not implemented');
+
+  // Create connection to database
+  const jateDb = await openDB('jate', 1);
+  // Create a transaction by transaction method from database that takes in name of db and the action we want to perform
+  const tx = jateDb.transaction('jate', 'readonly');
+  // Open up the desired object store.
+  const store = tx.objectStore('jate');
+  // Use the .getAll() method to get all data in the database
+  const request = store.getAll();
+  // Get confirmation of the request
+  const result = await request;
+  console.log('result.value', result);
+  return result;
+}
 
 initdb();
